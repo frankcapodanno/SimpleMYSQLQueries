4cb3adeb8db6b8ba8c96328a31a32578ac6cabe3
guru <guru@kali-guru.tld>
2016-09-23 15:38:47 +0200

    First Commit UploadFiles

--- src/simplemysqlqueries/SimpleMYSQLQueries.java
+++ src/simplemysqlqueries/SimpleMYSQLQueries.java
@@ -22,15 +22,17 @@ public class SimpleMYSQLQueries {
         System.out.println("This program collect a simple library for JDBC Connection on DB");
         System.out.println("Please before initalize the DB and change connectionAndQueries.ConnectionDB ");
         
-        System.out.println("Test inseriment of utente_test ...");
+        System.out.println("Test add a new empty document for the user utente_test ...");
         // new User Insert
         Insert1ValueNullId newUserInDb = new Insert1ValueNullId("utente_test",SimpleData.UNAME_COL, SimpleData.TABLE);
         if (newUserInDb.getError())
         {
             System.out.println("This query find an Error....");
             System.out.println(newUserInDb.getErrorString());
-        } else { System.out.print("Nessun errore in questa query:");
+        } else { System.out.print("No errors in this query:");
         System.out.println(newUserInDb.getQuery());}
+        
+        
     }
     
 }
